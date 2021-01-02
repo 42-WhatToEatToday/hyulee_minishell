c1607df88817c516be7202bca9b5be3f3b6564c4
hyulee <hyulee@student.42seoul.kr>
2021-01-02 23:21:39 +0900

    fix: cd after change PWD env

--- srcs/ft_cd.c
+++ srcs/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: hyulee <hyulee@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/31 22:24:36 by hyulee            #+#    #+#             */
-/*   Updated: 2021/01/02 13:13:07 by kyoukim          ###   ########.fr       */
+/*   Updated: 2021/01/02 23:20:58 by hyulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ extern	t_state *g_state;
 
 void	ft_cd(t_state *s, t_cmd cmd)
 {
+	char buf[MAX_PATH_LENGTH];
+
 	if (cmd.argv[1] == NULL || ft_strcmp(cmd.argv[1], "~") == 0)
 		cmd.argv[1] = find_env(&(s->env_head), "HOME")->value;
 	if (chdir(cmd.argv[1]) == -1)
@@ -23,4 +25,6 @@ void	ft_cd(t_state *s, t_cmd cmd)
 		ft_printf("sh: %s: %s: No such file or directory\n", cmd.argv[0], cmd.argv[1]);
 		s->exitnum = 22;
 	}
+	getcwd(buf, MAX_PATH_LENGHT);
+	change_env(&(s->env_head), "PWD", buf);
 }
