35c797b68df06e7e056dd58b19548390442a4442
hyulee <hyulee@student.42seoul.kr>
2021-01-07 02:35:24 +0900

    fix: binary in PATH exec

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: hyulee <hyulee@student.42.kr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/24 01:45:03 by hyulee            #+#    #+#             */
-/*   Updated: 2021/01/07 00:25:10 by kyoukim          ###   ########.fr       */
+/*   Updated: 2021/01/07 02:20:59 by hyulee           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,7 @@
 # include <signal.h>
 # include <errno.h>
 # include <sys/wait.h>
+# include <sys/stat.h>
 # include <sys/types.h>
 # include <sys/resource.h>
 # include <sys/time.h>
@@ -122,6 +123,7 @@ void	execute(t_state *s, char **envp);
 int		get_argv_num(t_cmd cmd);
 
 char	**parse_path(t_state *s);
+int		check_path(t_state *s, t_cmd *cmd);
 
 int		execute_redirection(t_state *s, char *c, int *rd, int *wrt);
 int		search_token(t_state *s, char *c);
